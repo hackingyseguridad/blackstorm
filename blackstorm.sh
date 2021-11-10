9cabd0aca1cb853cf97e63848920adf7c57dcd1a
hackingyseguridad <antonio_taboada@hotmail.es>
2021-11-10 19:21:41 +0100

    Update blackstorm.sh

--- blackstorm.sh
+++ blackstorm.sh
@@ -1,9 +1,10 @@
 # !/bin/bash
 # Herramienta de test de stress ICMP type 3 code 3 a partir de paquetes UDP elaborados con Hping3
 # (C) hackingyseguridad.com 2021
+# chmod 777 blackstorm.sh
 # Uso: sh blackstorm.sh ip_target puerto_target ip_spoof
-echo
-echo UDP Flood con herramienta hping3
+echo 
+echo "UDP Flood con herramienta hping3 reflexión ICMP type 3 code 3"
 echo "Uso: ./blackstorm.sh IP Puerto_UDP IP_spoof"
 echo "Control + C para interrumpir"
 echo 

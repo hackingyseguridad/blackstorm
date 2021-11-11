d2a26871cb1bd8b47ea01c8c9692842b6a44130e
hackingyseguridad <antonio_taboada@hotmail.es>
2021-11-11 17:04:53 +0100

    Update respuesta.sh

--- respuesta.sh
+++ respuesta.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# Respuesta ICMP Tipo 3 Code 3 al enviar paquetes a puertos UDP filtrados
+# Script para ver respuesta ICMP Tipo 3 Code 3 al enviar paquetes a puertos UDP filtrados, en las IP del fichero ips.txt
 for n in `cat ips.txt`; do echo $n; hping3 -2 $n -p 53 -c 1 |grep ICMP; done
